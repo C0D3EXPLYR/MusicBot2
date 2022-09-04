527a8d181b5c4ad5aca4d6b9f2c3b7e92dab8538
C0D3EXPLYR <101raluger.s0r1s@gmail.com>
2022-09-04 12:54:11 +0200

    update

---
+++ slash/info.js
@@ -0,0 +1,23 @@
+const {SlashCommandBuilder}= require("@discordjs/builders")
+const {MessageEmbed}= require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder().setName("info").setDescription("Displays info about the current song just playing"),
+    run: async ({client, interaction}) => {
+        const queue= client.player.getQueue(interaction.guildId)
+
+        if(!queue) return await interaction.editReply("There are no songs in the queue")
+
+        let bar= queue.createProgressBar({
+            queue: false,
+            length: 19  
+        })
+        await interaction.editReply({
+            embeds: [new MessageEmbed()
+            .setThumbnail(song.thumbnail)
+            .setDescription(`Currently playing [${song.title}](${song.url})\n\n` + bar)
+        ],
+        })
+        await interaction.editReply("Leaving voice")
+    },
+}
